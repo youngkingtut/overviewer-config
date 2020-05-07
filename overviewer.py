534e353100ec594b19211cb3e0a113399952ac7b
youngkingtut <tristanstorz@gmail.com>
2020-05-06 20:21:36 -0600

    overviewer does not allow vars in config, moved all string constants in functions.

--- overviewer.py
+++ overviewer.py
@@ -1,37 +1,27 @@
-URL = "http://www.brohangwangbang.party"
-TOWN_MARKER = "TownMarker"
-NETHER_GATE_MARKER = "NetherMarker"
-
-TOWN_ICON = f"{URL}/icons/marker_town.png"
-NETHER_ICON = f"{URL}/icons/marker_town.png"
-DEFAULT_PLAYER_ICON = f"{URL}/icons/marker_location.png"
-PLAYER_ICONS = {
-    "kylehighclub": f"{URL}/icons/kyle.png",
-    "HallsaUsageway": f"{URL}/icons/mike.png",
-    "brjns": f"{URL}/icons/levi.png",
-    "Bromentum": f"{URL}/icons/bromax.png",
-    "IamYourFarter": f"{URL}/icons/matt.png",
-    "chucklebug": f"{URL}/icons/chuck.png",
-    "tutenkhamun": f"{URL}/icons/tristan.png"
-}
-
-
 def town_filter(poi):
     if (poi["id"] == "Sign" or poi["id"] == "minecraft:sign") and poi["Text1"] == "TownMarker":
-        poi["id"] = TOWN_ICON
+        poi["id"] = "http://www.brohangwangbang.party/icons/marker_town.png"
         return " ".join([poi["Text2"], poi["Text3"], poi["Text4"]])
 
 
 def nether_filter(poi):
     if (poi["id"] == "Sign" or poi["id"] == "minecraft:sign") and poi["Text1"] == "NetherMarker":
-        poi["id"] = NETHER_ICON
+        poi["id"] = "http://www.brohangwangbang.party/icons/marker_town.png"
         return " ".join([poi["Text2"], poi["Text3"], poi["Text4"]])
 
 
 def player_filter(poi):
     if poi["id"] == "Player":
         player_name = poi["EntityId"]
-        poi["icon"] = PLAYER_ICONS.get(player_name, DEFAULT_PLAYER_ICON)
+        poi["icon"] = {
+            "kylehighclub": f"http://www.brohangwangbang.party/icons/kyle.png",
+            "HallsaUsageway": f"http://www.brohangwangbang.party/icons/mike.png",
+            "brjns": f"http://www.brohangwangbang.party/icons/levi.png",
+            "Bromentum": f"http://www.brohangwangbang.party/icons/bromax.png",
+            "IamYourFarter": f"http://www.brohangwangbang.party/icons/matt.png",
+            "chucklebug": f"http://www.brohangwangbang.party/icons/chuck.png",
+            "tutenkhamun": f"http://www.brohangwangbang.party/icons/tristan.png"
+        }.get(player_name, "http://www.brohangwangbang.party/icons/marker_location.png")
         return player_name
 
 
@@ -44,7 +34,8 @@ renders["surface"] = {
     "markers": [
         dict(name="players", filterFunction=player_filter, checked=True),
         dict(name="towns", filterFunction=town_filter, checked=True)
-    ]
+    ],
+    "showspawn": False
 }
 renders["nether"] = {
     "world": "BroHangWangBang",
